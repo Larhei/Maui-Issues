0da05687047071645c2c2e91c2e3a816f7eda755
Larhei <lh@heires.net>
2022-11-22 13:59:03 +0100

    Inital checkin

---
+++ ScrollViewer/ScrollViewer/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace ScrollViewer;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		this.BindingContext = new MainPageViewModel();
+	}
+
+	private MainPageViewModel ViewModel
+    {
+		get
+		{
+			return this.BindingContext as MainPageViewModel;
+
+        }
+	}
+
+    void ContentPage_Loaded(object sender, System.EventArgs e)
+    {
+		ViewModel.LoadData(Dispatcher);
+
+    }
+}
+
+

ce0cf869dc22941811662b7c4525ff6e5f21f49e
Larhei <47631685+Larhei@users.noreply.github.com>
2023-07-02 18:38:12 +0200

    Inital checkin

---
+++ ParentLayoutIssues/ParentLayoutIssues/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ParentLayoutIssues;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ ParentLayoutIssues/ParentLayoutIssues/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ParentLayoutIssues;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    async void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Issue1");
+    }
+
+}
+
+

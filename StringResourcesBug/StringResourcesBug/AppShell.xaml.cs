72b0ba302cdaebf1e438318895c1667b02fe7385
Larhei <47631685+Larhei@users.noreply.github.com>
2023-11-29 18:16:23 +0100

    Initial commit

---
+++ StringResourcesBug/StringResourcesBug/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace StringResourcesBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		var x = StringResourcesBug.Resources.Strings.Resource.Test;
+	}
+}
+

f665e89426265fe9f7efe22c21c6edc270cada96
Lars Heinrich <lh@heires.net>
2024-08-20 22:38:21 +0200

    initial checkin

---
+++ ListVirtualisation/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ListVirtualisation
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ListVirtualisation/MyViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListVirtualisation
+{
+
+    public partial class MyViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<string> _items = new ObservableCollection<string>() {
+            
+        };
+
+        public MyViewModel() 
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _items.Add(i.ToString());
+            }
+        }
+    }
+}

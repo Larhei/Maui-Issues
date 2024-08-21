35ce5202d369eff572a8225a4ab93922e8b5f232
Lars Heinrich <lh@heires.net>
2024-08-21 11:44:27 +0200

    Initial checkin

---
+++ ScrollToNullPointer/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ScrollToNullPointer
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MyViewModel(this.Dispatcher, PART_Scroll_ChildAdded);
+        }
+
+        private void PART_Scroll_ChildAdded(object item)
+        {
+            Dispatcher.Dispatch(() => PART_Scroll.ScrollTo(item, ScrollToPosition.MakeVisible, true));
+        }
+    }
+
+}

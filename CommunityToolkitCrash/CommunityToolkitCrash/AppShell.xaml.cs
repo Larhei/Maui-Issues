2738452716f9332f6b1fa9ff34636910084299ba
Larhei <47631685+Larhei@users.noreply.github.com>
2024-08-28 23:06:02 +0200

    Initial checkin

---
+++ CommunityToolkitCrash/CommunityToolkitCrash/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CommunityToolkitCrash;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ CommunityToolkitCrash/CommunityToolkitCrash/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CommunityToolkitCrash;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+

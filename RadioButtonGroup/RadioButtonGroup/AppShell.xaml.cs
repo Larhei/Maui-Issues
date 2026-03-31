2839aeea62f125b816c2c437cfec2d55d67fdd33
Lars Heinrich <lh@heires.net>
2026-03-31 16:43:52 +0200

    Added RadioButtonGroup Sample

---
+++ RadioButtonGroup/RadioButtonGroup/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RadioButtonGroup
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RadioButtonGroup/RadioButtonGroup/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RadioButtonGroup
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
